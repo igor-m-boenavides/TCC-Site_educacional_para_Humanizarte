d1c32af10beddae2d649ae7f24a7abb26ffb1852
Igy01 <igymorboe@gmail.com>
2023-05-31 14:40:19 -0300

    commit

--- PROFESSOR/suas turmas/cadastroTurma.php
+++ PROFESSOR/suas turmas/cadastroTurma.php
@@ -7,19 +7,20 @@ $password = "";
 $dbname = "humanizarte";
 
 // Variáveis do formulário
+$id = $_POST['id'];
 $nome = $_POST['nome'];
-$email = $_POST['email'];
-$senha = $_POST['senha'];
-$telefone = $_POST['telefone'];
+$desc = $_POST['desc'];
+$quantidade = $_POST['quantidade'];
 
 try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $sql = "INSERT INTO aluno (nome, email, senha, telefone) VALUES ('$nome', '$email', '$senha', '$telefone')";
+
+  $sql = "INSERT INTO curso (id, nome, descricao, quantidade) VALUES ('$id', '$nome', '$desc', '$quantidade')";
   // use exec() because no results are returned
   $conn->exec($sql);
-  echo "Aluno cadastrado com sucesso!";
+  echo "Curso cadastrado com sucesso!";
 } catch(PDOException $e) {
   echo $sql . "<br>" . $e->getMessage();
 }
