767d8e7b9f7d5e37539e91921b2f4905ee7e457e
Igy01 <igymorboe@gmail.com>
2023-07-04 08:22:36 -0300

    carrosel adicionado e navbar corrigida

--- .modelagem_bdd/humanizarte.sql
+++ .modelagem_bdd/humanizarte.sql
@@ -1,49 +1,53 @@
-CREATE DATABASE humanizarte;
+CREATE DATABASE IF NOT EXISTS humanizarte;
 
 USE humanizarte;
 
-DROP TABLE IF EXISTS curso;
-CREATE TABLE IF NOT EXISTS humanizarte.curso (
-    id INT NOT NULL,
+DROP TABLE IF EXISTS turma;
+CREATE TABLE IF NOT EXISTS turma (
+    id INT NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (id),
     nome VARCHAR(200) NOT NULL,
     descricao TEXT NOT NULL
 );
 
 DROP TABLE IF EXISTS aluno;
-CREATE TABLE IF NOT EXISTS humanizarte.aluno (
+CREATE TABLE IF NOT EXISTS aluno (
     id INT NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (id),
     nome VARCHAR(200) DEFAULT NULL,
     email VARCHAR(200) DEFAULT NULL,
     senha VARCHAR(200) DEFAULT NULL,
     telefone VARCHAR(200) DEFAULT NULL,
-    curso_id INT,
-    FOREIGN KEY (curso_id)
-        REFERENCES curso (id)
+    turma_id INT,
+    FOREIGN KEY (turma_id)
+        REFERENCES turma (id)
+);
+
+DROP TABLE IF EXISTS aluno_turma;
+CREATE TABLE IF NOT EXISTS aluno_turma (
+    aluno_id INT,
+    turma_id INT,
+    FOREIGN KEY (aluno_id)
+        REFERENCES aluno (id),
+    FOREIGN KEY (turma_id)
+        REFERENCES turma (id),
+    INDEX fk_aluno_idx (aluno_id),
+    INDEX fk_turma_idx (turma_id)
 );
 
 DROP TABLE IF EXISTS aula;
-CREATE TABLE IF NOT EXISTS humanizarte.aula (
+CREATE TABLE IF NOT EXISTS aula (
     id INT NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (id),
     nome VARCHAR(200) DEFAULT NULL,
     descricao TEXT DEFAULT NULL,
     video VARCHAR(200) DEFAULT NULL,
     anexo VARCHAR(200) DEFAULT NULL,
-    curso_id INT,
-    FOREIGN KEY (curso_id)
-        REFERENCES humanizarte.curso (id)
+    turma_id INT,
+    FOREIGN KEY (turma_id)
+        REFERENCES turma (id)
 );
 
-SELECT * FROM humanizarte.curso;
-SELECT * FROM humanizarte.aluno;
-SELECT * FROM humanizarte.aula;
-
-
-
-
-
-
-
-
+SELECT * FROM turma;
+SELECT * FROM aluno;
+SELECT * FROM aula;
--- PROFESSOR/alunos/cadastroAluno.php
+++ PROFESSOR/alunos/cadastroAluno.php
@@ -11,28 +11,49 @@ $nome = $_POST['nome'];
 $email = $_POST['email'];
 $senha = $_POST['senha'];
 $telefone = $_POST['telefone'];
+$turma = $_POST['turma'];
 
 // ...
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  
-  // Preparar a declaração SQL
-  $stmt = $conn->prepare("INSERT INTO aluno (nome, email, senha, telefone) VALUES (:nome, :email, :senha, :telefone)");
-  
-  // Vincular os valores dos parâmetros
-  $stmt->bindParam(':nome', $nome);
-  $stmt->bindParam(':email', $email);
-  $stmt->bindParam(':senha', $senha);
-  $stmt->bindParam(':telefone', $telefone);
-  
-  // Executar a declaração
-  $stmt->execute();
-  
-  echo "Aluno cadastrado com sucesso!";
-} catch(PDOException $e) {
-  echo "Erro: " . $e->getMessage();
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Preparar a declaração SQL
+    $stmt = $conn->prepare("INSERT INTO aluno (nome, email, senha, telefone) VALUES (:nome, :email, :senha, :telefone)");
+
+    // Vincular os valores dos parâmetros
+    $stmt->bindParam(':nome', $nome);
+    $stmt->bindParam(':email', $email);
+    $stmt->bindParam(':senha', $senha);
+    $stmt->bindParam(':telefone', $telefone);
+
+    // Executar a declaração
+    $stmt->execute();
+
+    // Get the ID of the last inserted record
+    $aluno_id = $conn->lastInsertId();
+
+    // Insert turma data into a separate table
+    foreach ($turma as $t) {
+        $stmt = $conn->prepare("INSERT INTO aluno_turma (aluno_id, turma_id) VALUES (:aluno_id, :turma_id)");
+        $stmt->bindParam(':aluno_id', $aluno_id);
+        $stmt->bindParam(':turma_id', $t);
+        $stmt->execute();
+    }
+
+    echo "Aluno cadastrado com sucesso!";
+} catch (PDOException $e) {
+    if ($e->getCode() == '23000') {
+        $errorInfo = $e->errorInfo;
+        if ($errorInfo[1] == 1452) {
+            echo "Foreign key constraint violation: " . $errorInfo[2];
+        } else {
+            echo "Database error: " . $e->getMessage();
+        }
+    } else {
+        echo "General error: " . $e->getMessage();
+    }
 }
 
 $conn = null;
